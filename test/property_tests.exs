ad2256eb102fbd1fcbeab4045a41c99384f11767
Amos L King <amos@binarynoggin.com>
2018-05-03 18:45:37 -0500

    Adds testing for recv_bin
    This test is currently failing
    
    Amos King @adkron <amos@binarynoggin.com>

--- test/property_tests.exs
+++ test/property_tests.exs
@@ -3,11 +3,23 @@ defmodule PropertyTests do
   use PropCheck
   use ExUnit.Case, async: false
 
-  @default_dir "/"
+  @default_dir '/'
+  @test_dir './_tmp_server'
   @test_port 2525
+  @initial_filename 'initial_file.txt'
+  @initial_file_in_ftp [@default_dir | ['/' | @initial_filename]]
+  @initial_file [@test_dir | ['/' | @initial_filename]]
+  @initial_file_content "initial content"
 
   require Logger
   @moduletag capture_log: true
+
+  setup do
+    File.rm_rf(@test_dir)
+    File.mkdir_p(@test_dir)
+    :ok = File.write(@initial_file, @initial_file_content)
+    :ok
+  end
 
   property "ftp connections" do
     numtests(
@@ -35,7 +47,7 @@ defmodule PropertyTests do
     )
   end
 
-  defstruct connected: false, authenticated: false, inets_pid: :disconnected
+  defstruct connected: false, authenticated: false, inets_pid: :disconnected, pwd: @default_dir, files: [{@initial_file_in_ftp, @initial_file_content}]
 
   def initial_state, do: %__MODULE__{}
 
@@ -52,6 +64,10 @@ defmodule PropertyTests do
     :inets.stop(:ftpc, pid)
   end
 
+  def filename do
+
+  end
+
   def command(%{inets_pid: :disconnected}) do
     {:call, __MODULE__, :connect, []}
   end
@@ -66,7 +82,8 @@ defmodule PropertyTests do
   def command(%{authenticated: true, connected: true, inets_pid: pid}) do
     oneof([
       {:call, __MODULE__, :disconnect, [pid]},
-      {:call, :ftp, :send_bin, [pid, 'Woo dogs', 'filename.tmp']},
+      {:call, :ftp, :recv_bin, [pid, @initial_filename]},
+      {:call, :ftp, :send_bin, [pid, "Woo dogs", 'filename.tmp']},
       {:call, :ftp, :pwd, [pid]}
     ])
   end
@@ -107,9 +124,18 @@ defmodule PropertyTests do
     false
   end
 
-  def postcondition(_, {:call, :ftp, :send_bin, [_pid, contents, filename]}, :ok) do
-    file_path = "#{@default_dir}/#{filename}"
-    File.exists?(file_path) and File.read(file_path) == {:ok, contents}
+  def postcondition(_, {:call, :ftp, :send_bin, [pid, contents, filename]}, :ok) do
+    case :ftp.recv_bin(pid, filename) do
+      {:ok, bin} ->
+        if contents != bin do
+          IO.puts "expected #{inspect filename} to have contents #{inspect bin}, but got #{inspect contents}"
+        else
+          true
+        end
+      error ->
+        IO.puts "Failed to recv_bin #{inspect error} pid: #{inspect pid} file: #{inspect filename}"
+        false
+    end
   end
 
   def postcondition(%{connected: false}, {:call, __MODULE__, :connect, []}, pid) when is_pid(pid), do: true
